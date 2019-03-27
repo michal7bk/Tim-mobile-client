82a081ab5bd039c1cc425d79f5249140250646f4
mbak <michal.bak@impaqgroup.com>
2019-03-27 22:02:14 +0100

    customer -> list coaches and ask for contact

---
+++ app/src/main/java/pl/michal/tim_client/customer/CoachesArrayAdapter.java
@@ -0,0 +1,86 @@
+package pl.michal.tim_client.customer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import pl.michal.tim_client.R;
+import pl.michal.tim_client.domain.Coach;
+import pl.michal.tim_client.utils.Connection;
+import pl.michal.tim_client.utils.ObjRequestWithToken;
+
+import java.util.List;
+
+public class CoachesArrayAdapter extends ArrayAdapter<Coach> {
+    private final String TAG = "CoachesArrayAdapter";
+
+
+    private Context context;
+    private int layoutResourceId;
+    private List<Coach> coaches;
+
+    CoachesArrayAdapter(Context context, int layoutResourceId,
+                        List<Coach> studs) {
+        super(context, layoutResourceId, studs);
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.coaches = studs;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View item = convertView;
+        CoachWrapper coachWrapper;
+        if (item == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            item = inflater.inflate(layoutResourceId, parent, false);
+            coachWrapper = new CoachWrapper();
+            coachWrapper.name = item.findViewById(R.id.textName_coachesList);
+            coachWrapper.contact = item.findViewById(R.id.btnContact);
+            item.setTag(coachWrapper);
+        } else {
+            coachWrapper = (CoachWrapper) item.getTag();
+        }
+        Coach coach = coaches.get(position);
+        coachWrapper.name.setText(coach.getName()+ " " + coach.getSurname());
+        coachWrapper.contact.setText("Ask for contact");
+
+        coachWrapper.contact.setOnClickListener(v -> {
+            askForContact(coach);
+            Toast.makeText(context,"Your ask was send, wait for answer from coach" , Toast.LENGTH_LONG).show();
+        });
+        return item;
+    }
+
+    static class CoachWrapper {
+        TextView name;
+        TextView surname;
+        TextView email;
+        Button contact;
+    }
+
+
+    private void askForContact(Coach coach) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = Connection.url + "/customers/" + Connection.getUser().getId() + "/ask-for-contact?email=" + coach.getEmail();
+        Log.i(TAG, "Making request on : " + url);
+        ObjRequestWithToken getRequest = new ObjRequestWithToken(Request.Method.GET, url, null,
+                response -> {
+
+                },
+                error -> Log.d("Error.Response", String.valueOf(error))
+        );
+        queue.add(getRequest);
+    }
+
+
+}
---
+++ app/src/main/java/pl/michal/tim_client/customer/CustomerFindCoachActivity.java
@@ -0,0 +1,107 @@
+package pl.michal.tim_client.customer;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.JSONException;
+import org.json.JSONObject;
+import pl.michal.tim_client.R;
+import pl.michal.tim_client.domain.Coach;
+import pl.michal.tim_client.domain.Customer;
+import pl.michal.tim_client.user.User;
+import pl.michal.tim_client.utils.ArrRequestWithToken;
+import pl.michal.tim_client.utils.Connection;
+import pl.michal.tim_client.utils.LocalDateTimeJsonConverter;
+import pl.michal.tim_client.utils.ObjRequestWithToken;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerFindCoachActivity extends AppCompatActivity {
+    Customer customer;
+    private final String TAG = "CustomerFindCoachActivity";
+    private CoachesArrayAdapter coachesArrayAdapter;
+    private ListView listView;
+    private List<Coach> coaches = new ArrayList<>();
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_coaches_list);
+        ButterKnife.bind(this);
+        matchCustomerWithUser(Connection.getUser());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void getCoaches() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String acceptedUrl = Connection.url + "/coaches";
+        Log.i(TAG, "Making request on : " + acceptedUrl);
+        ArrRequestWithToken getAccepted = new ArrRequestWithToken(Request.Method.GET, acceptedUrl, null,
+                response -> {
+                    Gson gson = new GsonBuilder()
+                            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeJsonConverter())
+                            .serializeNulls()
+                            .create();
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject coach = response.getJSONObject(i);
+                            coaches.add(gson.fromJson(String.valueOf(coach), Coach.class));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    populateTable(coaches);
+                },
+                error -> Log.d("Error.Response", String.valueOf(error))
+        );
+        queue.add(getAccepted);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void populateTable(List<Coach> coaches) {
+        coachesArrayAdapter = new CoachesArrayAdapter(
+                CustomerFindCoachActivity.this, R.layout.list_item_coaches_list, coaches);
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(coachesArrayAdapter);
+        listView.setItemsCanFocus(false);
+        listView.setOnItemClickListener(
+                (parent, view, position, id) -> Toast.makeText(
+                        CustomerFindCoachActivity.this, "List Item Clicked: " + position, Toast.LENGTH_LONG).show());
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void matchCustomerWithUser(User user) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = Connection.url + "/customers/" + user.getId();
+        Log.i(TAG, "Making request on : " + url);
+        ObjRequestWithToken getRequest = new ObjRequestWithToken(Request.Method.GET, url, null,
+                response -> {
+                    Gson gson = new GsonBuilder()
+                            .serializeNulls()
+                            .setPrettyPrinting()
+                            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeJsonConverter())
+                            .create();
+                    customer = gson.fromJson(String.valueOf(response), Customer.class);
+                    Log.i(TAG, "Read customer : " + customer);
+                    getCoaches();
+                },
+                error -> Log.d("Error.Response", String.valueOf(error))
+        );
+        queue.add(getRequest);
+    }
+}
